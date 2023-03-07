afbee662ff17e5c25d29e5e1d9c770c69c98a672
anukrishnanythik <anukrishnaanuz@gmail.com>
2023-03-07 16:14:11 +0530

    admin and user template set

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\homeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,9 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [homeController::class,"index"]);
+
 
 Route::middleware([
     'auth:sanctum',
